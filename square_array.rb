2b42e356077d4aad41f9e2920322a754bbbfb077
Matt Plichta <bigcatplichta@gmail.com>
2019-09-11 17:20:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,14 @@
-def square_array(array)
-  sq_array = []
+# def square_array(array)
+#   sq_array = []
   
-  array.each do | num |
-    sq_array.push(num ** 2) 
+#   array.each do | num |
+#     sq_array.push(num ** 2) 
+#   end
+#   sq_array
+# end
+
+def square_array(array)
+  array.collect do | num |
+    num ** 2 
   end
-  sq_array
 end
